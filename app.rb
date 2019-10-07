0530a601a633da287112eeefa7fbc6d7d13514fe
Michael McCullough <michael.mccullough21@gmail.com>
2019-10-07 15:23:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,8 +7,15 @@ get "/newteam" do
 end
 
 post "/newteam" do
-  @team = params
-  
+  @team_name = params[:team_name]
+  @coach = params[:coach]
+  @pg = params[:pg]
+  @sg = params[:sg]
+  @sf = params[:sf]
+  @pf = params[:pf]
+  @c = params[:c]
+
+
   erb :team
 end
 
